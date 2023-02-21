03e702b015c64ace9d03c40e66d34a9c43040fa4
abdelrahman <abdosobhy18220@gmail.com>
2023-02-21 17:03:16 +0200

    lastEdit

--- PredictionModel.py
+++ PredictionModel.py
@@ -1,34 +1,80 @@
-from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
 import pandas as pd
 import math
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+from sklearn.metrics import classification_report, r2_score
+import sklearn.metrics as sm
 
-def merge (data , dataframe):
+
+def merge(data, dataframe):
     result = pd.concat([data, dataframe], axis=1, join='inner')
     return result
 
 
+modelNum = linear_model.LinearRegression()
+
 
 ###### all data
-def trainModel (data , dataframe,classification):
+def trainModel(data, dataframe, classification):
     data.dropna(inplace=True)
-    X_Train = data.drop(dataframe, axis=1)
-    Y_train = data[dataframe]
+    X = data.drop(dataframe, axis=1)
+    Y = data[dataframe]
+    # print(dataframe)
+
+    feature = ""
     if classification is True:
         model = LogisticRegression()
-    else :
-        model = LinearRegression()
-    model.fit(X_Train, Y_train)
-    return  model
+        feature = selectFeatureChi2(X, Y)
+        X = data[feature]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=100)
+        model.fit(X_train, y_train)
+
+
+
+    else:
+        feature = selectFeatureCorr(data, dataframe)
+        X = data[feature]
+        print(X)
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=100)
+        # poly_features = PolynomialFeatures(degree=3)
+        # X_train_poly = poly_features.fit_transform(X_train)
+        # model = linear_model.LinearRegression()
+        # model.fit(X_train_poly, y_train)
+        modelNum.fit(X_train, y_train)
+
+    if classification is True:
+        y_pred = model.predict(X_test)
+        # classificationAccuracy(y_test,y_pred)
+        return model, feature
+    else:
+        # regressionAccuracy(y_test,y_pred)
+        print("Accuracy of training dataset:", modelNum.score(X_train, y_train))
+        print("Accuracy of test dataset:", modelNum.score(X_test, y_test))
+        # print('Coefficient of determination: %.2f' % r2_score(y_test, predict))
+        return modelNum, feature
+    # print(feature)
+
 
 # after merge
-def getTestData(data , dataframe):
+def getTestData(data, dataframe):
     TestData = data[data[dataframe].isnull()]
-    return  TestData
+    return TestData
 
-def predict(TestData , model ,data , dataframe):
-    X_Test = TestData.iloc[:, 0:13]
+
+def predict(TestData, selected, model, data, dataframe):
+    # print(TestData)
+    X_Test = TestData[selected]
+    # print(X_Test)
     Y_Pred = model.predict(X_Test)
     TestData['prediction'] = Y_Pred
     counter = 0
@@ -42,5 +88,54 @@ def predict(TestData , model ,data , dataframe):
     return data
 
 
+def selectFeatureChi2(X, Y):
+    best_features = SelectKBest(score_func=chi2, k=3)
+    fit = best_features.fit(X, Y)
+    df_scores = pd.DataFrame(fit.scores_)
+    df_columns = pd.DataFrame(X.columns)
+    features_scores = pd.concat([df_columns, df_scores], axis=1)
+    features_scores.columns = ['Features', 'Score']
+    features_scores = features_scores.sort_values(by='Score', ascending=False)
+    feature = features_scores['Features'].tolist();
+    # print(feature[0:3])
+    return feature[0:3]
 
 
+def selectFeatureCorr(data, Y):
+    corr = data.corr()
+    # print(data)
+    # Top 50% Correlation training features with the Value
+    top_feature = corr.index[abs(corr[Y]) > 0.4]
+    # Correlation plot
+    plt.subplots(figsize=(12, 8))
+    top_corr = data[top_feature].corr()
+    sns.heatmap(top_corr, annot=True)
+    plt.show()
+    top_corr = data[top_feature].corr()
+    top_feature = top_feature.delete(-1)
+    # print(len(top_feature))
+    # print(top_feature)
+    return top_feature
+
+
+def scale(X):
+    # Apply Standardization
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)
+    return X
+
+
+def classificationAccuracy(y_test, y_pred):
+    print('Accuracy:', metrics.accuracy_score(y_test, y_pred))
+    print('Recall: ', metrics.recall_score(y_test, y_pred, zero_division=1))
+    print('Precision:', metrics.precision_score(y_test, y_pred, zero_division=1))
+    print("CL Report:", metrics.classification_report(y_test, y_pred, zero_division=1))
+
+
+def regressionAccuracy(y_test, y_pred):
+    print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_pred), 2))
+    print("Mean squared error =", round(sm.mean_squared_error(y_test, y_pred), 2))
+    print("Median absolute error =", round(sm.median_absolute_error(y_test, y_pred), 2))
+    print("Explain variance score =", round(sm.explained_variance_score(y_test, y_pred), 2))
+    print("R2 score =", round(sm.r2_score(y_test, y_pred), 2))
--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from sklearn.linear_model import LogisticRegression
 import encoding
 import PredictionModel
 
-
-#read data
+# read data
 data = pd.read_csv('kidney_disease.csv')
 data.drop('id', axis=1, inplace=True)
 data.columns = ['age', 'blood_pressure', 'specific_gravity', 'albumin', 'sugar', 'red_blood_cells', 'pus_cell',
@@ -28,28 +27,28 @@ for col in cat_cols:
     data[col] = data[col].str.rstrip()
     data[col] = data[col].str.lstrip()
 #######################################################
-Selected = ['age','blood_pressure','blood_urea','serum_creatinine' ,'pus_cell_clumps' , 'bacteria' , 'hypertension', 'diabetes_mellitus' , 'coronary_artery_disease'
-,'appetite','peda_edema','aanemia' , 'class' , ]
+Selected = ['age', 'blood_pressure', 'blood_urea', 'serum_creatinine', 'pus_cell_clumps', 'bacteria', 'hypertension',
+            'diabetes_mellitus', 'coronary_artery_disease'
+    , 'appetite', 'peda_edema', 'aanemia', 'class', ]
 
-CDataCol = ['age','blood_pressure','blood_urea','serum_creatinine']
-CDataCat = ['pus_cell_clumps' , 'bacteria' , 'hypertension', 'diabetes_mellitus' , 'coronary_artery_disease'
-,'appetite','peda_edema','aanemia' , 'class' ,]
+CDataCol = ['age', 'blood_pressure', 'blood_urea', 'serum_creatinine']
+CDataCat = ['pus_cell_clumps', 'bacteria', 'hypertension', 'diabetes_mellitus', 'coronary_artery_disease'
+    , 'appetite', 'peda_edema', 'aanemia', 'class', ]
 
-test_list = ['specific_gravity','albumin', 'sugar','red_blood_cells','pus_cell','blood_glucose_random','sodium','potassium' ,
-             'haemoglobin','packed_cell_volume','white_blood_cell_count','red_blood_cell_count']
-CategralTest = ['red_blood_cells','pus_cell']
-NumaricTest=['specific_gravity','albumin', 'sugar','blood_glucose_random','sodium','potassium' ,
-             'haemoglobin','packed_cell_volume','white_blood_cell_count','red_blood_cell_count']
+test_list = ['specific_gravity', 'albumin', 'sugar', 'red_blood_cells', 'pus_cell', 'blood_glucose_random', 'sodium',
+             'potassium',
+             'haemoglobin', 'packed_cell_volume', 'white_blood_cell_count', 'red_blood_cell_count']
+CategralTest = ['red_blood_cells', 'pus_cell']
+NumaricTest = ['specific_gravity', 'albumin', 'sugar', 'blood_glucose_random', 'sodium', 'potassium',
+               'haemoglobin', 'packed_cell_volume', 'white_blood_cell_count', 'red_blood_cell_count']
 ## create new data without selected
-test_data = data [test_list]
+test_data = data[test_list]
 
 test_data.to_csv("test data.csv", index=False)
 
 data2 = data
-#data2 = encoding.encode(data2)
+data2 = encoding.encode(data2)
 data2.to_csv("temp.csv", index=False)
-
-
 
 ############## mode
 for col in CDataCat:
@@ -57,39 +56,31 @@ for col in CDataCat:
 ############## encoding
 pre = LabelEncoder()
 for i in CDataCat:
-    data[i]= pre.fit_transform(data[i])
+    data[i] = pre.fit_transform(data[i])
 
 ################# median
 for col in CDataCol:
     data[col] = data[col].fillna(data[col].median())
 
-
-
-
 train_data = data[Selected]
 train_data.to_csv("train data.csv", index=False)
 ###################################################
 
+
 for i in CategralTest:
     result = PredictionModel.merge(train_data, data[i])
+    result.to_csv("result.csv", index=False)
     TestData = PredictionModel.getTestData(result, i)
-    model = PredictionModel.trainModel(result, i, True)
-    data = PredictionModel.predict(TestData, model, data, i)
+    model, selected = PredictionModel.trainModel(result, i, True)
+    data = PredictionModel.predict(TestData, selected, model, data, i)
 
 for i in NumaricTest:
     result = PredictionModel.merge(train_data, data[i])
     TestData = PredictionModel.getTestData(result, i)
-    model = PredictionModel.trainModel(result, i, False)
-    data = PredictionModel.predict(TestData, model, data, i)
-
-
-
+    model, selected = PredictionModel.trainModel(result, i, False)
+    data = PredictionModel.predict(TestData, selected, model, data, i)
+    newData = pd.concat([train_data, data[i]], axis=1, join='inner')
+    train_data= newData
+    #print(train_data)
 
 data.to_csv("AllDetails.csv", index=False)
-
-
-
-
-
-
-
